7c0db166c38ac900c069529d0ba022afee6af430
ngo huy <87854612+huyngo202@users.noreply.github.com>
2025-05-26 02:17:06 +0700

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,2 @@
+export DATABASECONNECTION="mongodb://localhost:27017"
+export SESSION_SECRET="session_secret"
---
+++ social-media-client/src/app/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer
+      className="page-footer font-small fixed-bottom"
+      style={{ backgroundColor: "#e3f2fd" }}
+    >
+      <div className="footer-copyright text-center py-3">
+        Made by Huy Ngo &lt;3
+      </div>
+    </footer >
+  );
+};
